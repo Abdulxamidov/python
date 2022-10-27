838406e97a0efa52957b45382fe44df3fa78e7ac
Sardorbek <116817837+Abdulxamidov@users.noreply.github.com>
2022-10-27 21:10:26 +0500

    yangi darslar yuklandi.
    1,2,3,4-darslar

---
+++ hello.py
@@ -0,0 +1,54 @@
+# 21/10/22
+# Mavzu: print, sinteks, amallar
+  
+
+# \ => backslash deyiladi
+# \n => qatorni 2 ga bo'ladi
+
+# print("Odami ersang, demagil odami,\nOniki, yo'q xalq g'amidin g'ami")
+# Natija: 
+# Odami ersang, demagil odami,
+# Oniki, yo'q xalq g'amidin g'ami
+ 
+# """ => 3 ta qo'shtirnoq matn boshida va oxrida kelsa ham 2 ga bo'ladi :
+
+# print("""Odami ersang, demagil odami,
+# Oniki, yo'q xalq g'amidin g'ami""")
+# Natija: 
+# Odami ersang, demagil odami,
+# Oniki, yo'q xalq g'amidin g'ami
+
+# Arifmetik amallar:
+# print("To'qqizning kvadrati", 9**2, "ga teng")
+# Natija: To'qqizning kvadrati 81 ga teng
+
+# print("\"Nexia\" , \"Tico\" , 'Damas' , ko'rganlar qilar havas")
+# Natija:
+# "Nexia" , "Tico" , 'Damas' , ko'rganlar qilar havas
+
+# print('5 ning 4-darajasi' , 5**4 , 'ga teng') 
+# Natija:
+# 5 ning 4-darajasi 625 ga teng
+
+# 21/10/22
+# 4-dars
+# Mavzu: O'zgaruvchilar
+
+
+# ism = "Sardor"
+# yosh = 19
+# print(ism) => Sardor
+# print(yosh) => 19
+
+# a = 7
+# b = 8
+# c = (a*b)**4
+# print(c) => 9834496
+
+# ism_familiya = "Sardor Abdulxamidov"
+# print('Men' , ism_familiya, 'man') => Men Sardor Abdulxamidov man
+
+# Vazifa
+
+# salom = "Hello world!"
+# print(salom) => Hello world!
